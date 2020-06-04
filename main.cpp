95a1fd8e30c8fd5ad67de0fe41a725d10fbcc00d
Ksenia Rakcheeva <kseniiarakcheeva@yandex.ru>
2020-06-04 23:45:54 +0300

    Добавлен подсчет разности

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 	
 	printf("Input two numbers ");
     scanf("%d%d",&a,&b);
-	printf("%d + %d = %d ",a,b,a+b);
-	
+	printf("%d + %d = %d \n",a,b,a+b);
+	printf("%d - %d = %d ",a,b,a-b);
 	return 0;
 }
