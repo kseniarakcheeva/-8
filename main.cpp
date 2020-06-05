d93b302f4aef4382c064e8f6799762c1b306b1ab
Ksenia Rakcheeva <kseniiarakcheeva@yandex.ru>
2020-06-05 15:07:31 +0300

    Новое произведение

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ int main()
     scanf("%d%d",&a,&b);
 	printf("%d + %d = %d \n",a,b,a+b);
 	printf("%d - %d = %d \n",a,b,a-b);
-	printf("%d / %d = %d ",a,b,a/b);
+	printf("%d * %d = %d ",a,b,a*b);
 	return 0;
 }
